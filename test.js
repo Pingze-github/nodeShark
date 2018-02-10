67cb30c6fb2db5d0b2d7a64ae5a46bb8757e0ed3
wangshunzhang <wangshunzhang@henhaoji.com>
2018-02-10 15:16:22 +0800

    add promisified sqlite handler, with bulk function

--- test.js
+++ test.js
@@ -30,7 +30,7 @@ const proxySet = require('./lib/proxySet');
 // }();
 
 
-~async function () {
+async function test2() {
   const request = require('./lib/request');
   request({
     method: 'POST',
@@ -60,5 +60,37 @@ const proxySet = require('./lib/proxySet');
     .catch(err => {
       console.error(err);
     });
-}();
+}
 
+async function test3() {
+
+  const ERR_TYPES = {
+    right: 0,
+    not200: 1,
+    errored: 2,
+    timeout: 3
+  };
+
+  const DB = require('./lib/db');
+  const db = DB({
+    file: './lite.db'
+  });
+  await db.run('DROP TABLE IF EXISTS tbl_proxylog');
+  await db.run(`CREATE TABLE IF NOT EXISTS tbl_proxylog (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    url TEXT NOT NULL,
+    status INTEGER NOT NULL,
+    error INTEGER NOT NULL
+  )`);
+  await db.run('INSERT INTO tbl_proxylog VALUES (NULL, ?, ?, ?)',
+    ['http://www.baidu.com', 200, ERR_TYPES.right]);
+  await db.run('INSERT INTO tbl_proxylog VALUES (NULL, ?, ?, ?)',
+    ['http://localhost:3000/500', 500, ERR_TYPES.not200]);
+  await db.bulkRun('INSERT INTO tbl_proxylog VALUES (NULL, ?, ?, ?)', [
+    ['http://invalid', 0, ERR_TYPES.errored],
+    ['http://timout', 0, ERR_TYPES.timeout],
+  ]);
+  const proxylogs = await db.all('SELECT * FROM tbl_proxylog');
+  console.log(proxylogs);
+}
+test3();
